a6d5ef5636b4c17fe83e7edeedb4a16f35521329
zanonjonas <jonas.zanon@paxtecnologia.com.br>
2015-03-13 12:46:32 -0300

    zxcv

--- RelatorioPMAOutros/RelatoriosPMAVO/src/br/com/paxtecnologia/pma/relatorio/vo/DBSizeTabelaVO.java
+++ RelatorioPMAOutros/RelatoriosPMAVO/src/br/com/paxtecnologia/pma/relatorio/vo/DBSizeTabelaVO.java
@@ -1,6 +1,8 @@
 package br.com.paxtecnologia.pma.relatorio.vo;
 
-public class DBSizeTabelaVO {
+import java.util.Comparator;
+
+public class DBSizeTabelaVO implements Comparator<DBSizeTabelaVO>{
 	private String valor;
 	private String mes;
 	
@@ -12,14 +14,25 @@ public class DBSizeTabelaVO {
 		this.valor = valor;
 	}
 	
-	public String getMes() {
+	public String getData() {
 		return mes;
 	}
 	
-	public void setMes(String mes) {
+	public void setData(String mes) {
 		this.mes = mes;
 	}
 
-
+	@Override
+	public int compare(DBSizeTabelaVO positive, DBSizeTabelaVO negative) {
+		
+		String [] partsp = positive.getData().split("/");
+		String [] partsn = negative.getData().split("/");
+		
+		Integer pos = Integer.parseInt(partsp[1]+partsp[0].replaceAll("/", "")); 
+		Integer neg = Integer.parseInt(partsn[1]+partsn[0].replaceAll("/", ""));
+		
+		return pos - neg;
+		
+	}
 
 }
--- RelatorioPMAOutros/RelatoriosPMAVO/src/br/com/paxtecnologia/pma/relatorio/vo/TimeFrameVO.java
+++ RelatorioPMAOutros/RelatoriosPMAVO/src/br/com/paxtecnologia/pma/relatorio/vo/TimeFrameVO.java
@@ -1,6 +1,8 @@
 package br.com.paxtecnologia.pma.relatorio.vo;
 
-public class TimeFrameVO {
+import java.util.Comparator;
+
+public class TimeFrameVO implements Comparator<TimeFrameVO>{
 
 	private String data;
 	private Double valor;
@@ -19,6 +21,19 @@ public class TimeFrameVO {
 
 	public void setValor(Double valor) {
 		this.valor = valor;
+	}
+	
+	@Override
+	public int compare(TimeFrameVO positive, TimeFrameVO negative) {
+		
+		String [] partsp = positive.getData().split("/");
+		String [] partsn = negative.getData().split("/");
+		
+		Integer pos = Integer.parseInt(partsp[1]+partsp[0].replaceAll("/", "")); 
+		Integer neg = Integer.parseInt(partsn[1]+partsn[0].replaceAll("/", ""));
+		
+		return pos - neg;
+		
 	}
 
 }
